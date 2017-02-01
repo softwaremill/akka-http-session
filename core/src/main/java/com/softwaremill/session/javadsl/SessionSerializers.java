f862ad3160048a475b3d7c96bab98ed5c379b415
Jaroslaw Kijanowski <kijanowski@gmail.com>
2017-02-01 12:13:49 +0100

    double cast to prevent conversion issue from scala's Int to java's Integer resulting in an incompatible types error

--- core/src/main/java/com/softwaremill/session/javadsl/SessionSerializers.java
+++ core/src/main/java/com/softwaremill/session/javadsl/SessionSerializers.java
@@ -10,10 +10,10 @@ import scala.collection.immutable.Map;
 public final class SessionSerializers {
 
     public static final SessionSerializer<String, String> StringToStringSessionSerializer = SessionSerializer$.MODULE$.stringToStringSessionSerializer();
-    public static final SessionSerializer IntToStringSessionSerializer = SessionSerializer$.MODULE$.intToStringSessionSerializer();
-    public static final SessionSerializer LongToStringSessionSerializer = SessionSerializer$.MODULE$.longToStringSessionSerializer();
-    public static final SessionSerializer FloatToStringSessionSerializer = SessionSerializer$.MODULE$.floatToStringSessionSerializer();
-    public static final SessionSerializer DoubleToStringSessionSerializer = SessionSerializer$.MODULE$.doubleToStringSessionSerializer();
+    public static final SessionSerializer<Integer, String> IntToStringSessionSerializer = (SessionSerializer<Integer, String>) (SessionSerializer) SessionSerializer$.MODULE$.intToStringSessionSerializer();
+    public static final SessionSerializer<Long, String> LongToStringSessionSerializer = (SessionSerializer<Long, String>) (SessionSerializer) SessionSerializer$.MODULE$.longToStringSessionSerializer();
+    public static final SessionSerializer<Float, String> FloatToStringSessionSerializer = (SessionSerializer<Float, String>) (SessionSerializer) SessionSerializer$.MODULE$.floatToStringSessionSerializer();
+    public static final SessionSerializer<Double, String> DoubleToStringSessionSerializer = (SessionSerializer<Double, String>) (SessionSerializer) SessionSerializer$.MODULE$.doubleToStringSessionSerializer();
     public static final SessionSerializer<Map<String, String>, String> MapToStringSessionSerializer = SessionSerializer$.MODULE$.mapToStringSessionSerializer();
 
     private SessionSerializers() {
