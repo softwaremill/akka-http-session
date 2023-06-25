6833db9bea53a3e2ca863f57193599d3e3e0f8ff
Stéphane Manciot <stephane.manciot@gmail.com>
2023-06-25 11:22:49 +0200

    add generic endpoint to set a new Csrf Token and a session

--- core/src/main/scala/com/softwaremill/session/TapirEndpoints.scala
+++ core/src/main/scala/com/softwaremill/session/TapirEndpoints.scala
@@ -2,6 +2,7 @@ package com.softwaremill.session
 
 import sttp.model.Method
 import sttp.model.headers.CookieValueWithMeta
+import sttp.tapir.Endpoint
 import sttp.tapir.server.PartialServerEndpointWithSecurityOutput
 
 import scala.concurrent.Future
@@ -44,6 +45,27 @@ trait TapirEndpoints extends SessionEndpoints with CsrfEndpoints {
       optionalSession(sc, st)
     }
 
+  def setNewCsrfTokenWithSession[T, INPUT](
+      sc: TapirSessionContinuity[T],
+      st: SetSessionTransport,
+      checkMode: TapirCsrfCheckMode[T]
+  )(endpoint: => Endpoint[INPUT, Unit, Unit, Unit, Any])(implicit
+                                                         f: INPUT => Option[T])
+    : PartialServerEndpointWithSecurityOutput[(INPUT, Seq[Option[String]]),
+                                              Option[
+                                                T
+                                              ],
+                                              Unit,
+                                              Unit,
+                                              (Seq[Option[String]], Option[CookieValueWithMeta]),
+                                              Unit,
+                                              Any,
+                                              Future] =
+    setNewCsrfToken(checkMode) {
+      setSession(sc, st) {
+        endpoint
+      }
+    }
 }
 
 object TapirEndpoints extends TapirEndpoints
